06b7bc8c6d0f1492036fd3e02e22d490f97c71ee
Ari801 <159201079+Ari801@users.noreply.github.com>
2025-11-22 19:18:54 -0500

    Update conexion.php

--- catalogo_software/conexion.php
+++ catalogo_software/conexion.php
@@ -1,16 +1,18 @@
 <?php
+
 $host = "ba8nc4jg9bqtvynpltyd-mysql.services.clever-cloud.com";
 $dbname = "ba8nc4jg9bqtvynpltyd";
 $user = "uvha83bpisz5fttf";
-$password = "P5nKf8VdA8AVfos7OOy4";
-$port = "3306";
+$pass = "P5nKf8VdA8AVfos7OOy4";
 
 try {
-    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
-    exit;
+    die("Error de conexión: " . $e->getMessage());
 }
+
 ?>
+
+
 
