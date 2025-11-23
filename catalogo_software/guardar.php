5bea1ce162e35bd5ea879b2f63b1f6805e595f96
Ari801 <159201079+Ari801@users.noreply.github.com>
2025-11-22 19:19:24 -0500

    Update guardar.php

--- catalogo_software/guardar.php
+++ catalogo_software/guardar.php
@@ -8,12 +8,12 @@ $sql = "INSERT INTO softwares (nombre, descripcion) VALUES (:n, :d)";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':n', $nombre);
 $stmt->bindParam(':d', $descripcion);
-
 $stmt->execute();
 
 header("Location: index.php");
 exit();
 ?>
+
 
 
 
